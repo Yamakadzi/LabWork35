5a3c3edf377f83d043fde5ee2c71f9bb5e454afa
Yamakadzi <102765765+Yamakadzi@users.noreply.github.com>
2022-04-28 21:47:48 +0300

    LabWork35

---
+++ LabWork35/LabWork35/Task1/Pages/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "Главная";
+        }
+
+        private void AutorizationButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Pages.Autiruzation());
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Pages.Registration());
+        }
+    }
+}
